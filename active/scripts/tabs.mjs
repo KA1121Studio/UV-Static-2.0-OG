aa80e8d015a891ef69d1cc3e6b7d61ff43925069
K.A <akkazu1121@gmail.com>
2026-02-19 15:32:33 +0900

    Update tabs.mjs

--- active/scripts/tabs.mjs
+++ active/scripts/tabs.mjs
@@ -51,7 +51,7 @@ pageRefresh.onclick = () => {
 };
 
 newTabButton.onclick = () => {
-  addTab("harmonious-cendol-bf4e99.netlify.app");
+  addTab("https://harmonious-cendol-bf4e99.netlify.app");
 };
 
 // Options (opt menu)
@@ -133,7 +133,7 @@ const tabItem = (tab) => {
             if (tabs.length) focusTab(tabs[tabs.length - 1]);
             else
               setTimeout(() => {
-                addTab("harmonious-cendol-bf4e99.netlify.app");
+                addTab("https://harmonious-cendol-bf4e99.netlify.app");
               }, 100);
           }
 
@@ -248,7 +248,7 @@ async function addTab(link) {
   focusTab(tab);
 }
 
-addTab("harmonious-cendol-bf4e99.netlify.app");
+addTab("https://harmonious-cendol-bf4e99.netlify.app");
 
 const urlParams = new URLSearchParams(window.location.search);
 
